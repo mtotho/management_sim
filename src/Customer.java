ed2c25bb52c08d7080aaf118ad1ee3c61b1730f1
Patrick Welch <pjwelch@uvm.edu>
2014-02-12 14:27:16 -0500

    modified customer class slightly, fixed my build properties

--- src/Customer.java
+++ src/Customer.java
@@ -1,25 +1,51 @@
 //package restuarantsim;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Customer{
 
 private ArrayList<String> order;
+private int items;
+private int nextItem;
+private String  cb =  "cheeseburger";
+private String drink = "drink";
+private String fries = "fries";
 
 	//Constructor()
 	public Customer(){
-
-
 
 	}//end: Constructor()
 
 	public void getTraits(){
 
 
+
 	}//end getTraits
 
-	public String getOrder(){
+	public void giveOrder(){
 
-		return order.toString();
+		Random random = new Random();
+
+		items = random.nextInt() % 10;
+
+
+		for (int i = 0; i < items + 1; i++){
+
+			nextItem = ( random.nextInt() % 10 );
+			
+			
+
+
+		}
+
+
+
+
+	}//end giveOrder
+
+	public void getOrder(){
+
+		
 
 	}//end getOrder()
 
