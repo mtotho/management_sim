efac116e04bfb124fb48b44d41058f5ebea32c91
Patrick Welch <pjwelch@uvm.edu>
2014-04-23 14:40:46 -0400

    fixed issue with restaurant not loading money with game load

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -306,6 +306,7 @@ public class Restaurant{
 
 
 		timer.setTime(restaurantData.get(0).getTime());
+		money = restaurantData.get(0).getMoney();
 
 	//	for(int i = 0; i < restaurantData.size(); i++){
 
